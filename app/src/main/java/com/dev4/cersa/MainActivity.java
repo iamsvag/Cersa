bb22a320414418d848d5edee30f7baff0aec0dd8
svagenas <37613061+iamsvag@users.noreply.github.com>
2019-05-14 21:00:35 +0300

    -background -connection with main -Struct of Competitive

--- app/src/main/java/com/dev4/cersa/MainActivity.java
+++ app/src/main/java/com/dev4/cersa/MainActivity.java
@@ -1,13 +1,30 @@
 package com.dev4.cersa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button competitive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+// Menu Handling
+
+        competitive =  (Button)findViewById(R.id.Compbtn);
+            competitive.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, Competitive.class);
+                    startActivity(intent);
+                }
+            });
+
     }
 }
