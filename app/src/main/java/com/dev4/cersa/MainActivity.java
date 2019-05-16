719cfb574ec2a43c65ad07299d9417310a1bc286
Jim <freaknoob1@hotmail.com>
2019-05-16 12:15:15 +0300

    Addquestion XML kai listener gia dropdown koumpi

--- app/src/main/java/com/dev4/cersa/MainActivity.java
+++ app/src/main/java/com/dev4/cersa/MainActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Button competitive;
 
@@ -27,4 +30,51 @@ public class MainActivity extends AppCompatActivity {
             });
 
     }
+
+    private Spinner spinner;
+    private static final String[] paths = {"Calculus I", "Computer Architecture", "Software Engineering"};
+
+    @Override
+    protected void onCreate2(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addquestion);
+
+        spinner = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
+
+}
+
+
+
+
+
 }
