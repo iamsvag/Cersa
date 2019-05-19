9a9070394a73bdc5cb4fdd3f318aabc20aeed229
svagenas <37613061+iamsvag@users.noreply.github.com>
2019-05-19 23:09:33 +0300

    message timer

--- app/src/main/java/com/dev4/cersa/message.java
+++ app/src/main/java/com/dev4/cersa/message.java
@@ -1,13 +1,33 @@
 package com.dev4.cersa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class message extends AppCompatActivity {
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+
+        //delay for came to start
+
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override             public void run() {
+
+
+                Intent intent4 = new Intent(message.this, Competitive.class);
+                startActivity(intent4);                 finish();             }
+                },3000);
+
+
     }
 }
