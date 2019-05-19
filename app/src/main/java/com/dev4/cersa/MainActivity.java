bc937b33fff692ae960e1da8c1792d1570fd6cf3
Maniolas <NikosKonstanto7@yahoo.com>
2019-05-19 22:13:50 +0300

    mitsos

--- app/src/main/java/com/dev4/cersa/MainActivity.java
+++ app/src/main/java/com/dev4/cersa/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     Button competitive;
     Button practice;
     Button profile;
-
+    Button question;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         competitive = (Button) findViewById(R.id.Compbtn);
         practice = (Button) findViewById(R.id.Practbtn);
         profile = (Button) findViewById(R.id.profilebtn);
+        question = (Button) findViewById(R.id.questbtn);
 
         competitive.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddQuestion.class);
+                startActivity(intent);
+            }
+        });
     }
 }
