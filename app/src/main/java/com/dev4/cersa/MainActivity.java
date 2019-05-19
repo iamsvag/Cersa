ca0d7270b9cf148b437a052fc4afe0d615bd9ec9
svagenas <37613061+iamsvag@users.noreply.github.com>
2019-05-19 20:24:11 +0300

    svag

--- app/src/main/java/com/dev4/cersa/MainActivity.java
+++ app/src/main/java/com/dev4/cersa/MainActivity.java
@@ -4,23 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity   {
+public class MainActivity extends AppCompatActivity {
 
     Button competitive;
-    Button addquestion;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
 // Menu Handling
 
@@ -29,20 +22,10 @@ public class MainActivity extends AppCompatActivity   {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(MainActivity.this, Competitive.class);
+
                     startActivity(intent);
                 }
             });
 
-        addquestion =  (Button)findViewById(R.id.questbtn);
-        addquestion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddQuestion.class);
-                startActivity(intent);
-            }
-        });
     }
-
-
-
 }
