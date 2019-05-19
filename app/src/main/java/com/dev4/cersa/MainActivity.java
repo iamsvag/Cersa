2aeb2e68e2fe7b3eaa14ba211439a14accaa28d6
svagenas <37613061+iamsvag@users.noreply.github.com>
2019-05-19 19:30:21 +0300

    update

--- app/src/main/java/com/dev4/cersa/MainActivity.java
+++ app/src/main/java/com/dev4/cersa/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button competitive;
-    Button practice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,28 +18,14 @@ public class MainActivity extends AppCompatActivity {
 // Menu Handling
 
         competitive =  (Button)findViewById(R.id.Compbtn);
-        practice = (Button)findViewById(R.id.Practbtn);
-
             competitive.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, choose_course.class);
+                    Intent intent = new Intent(MainActivity.this, Competitive.class);
+
                     startActivity(intent);
                 }
             });
-
-            practice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, practice_course.class);
-                startActivity(intent);
-            }
-        });
-
-
-    }
-
-
 
     }
 }
